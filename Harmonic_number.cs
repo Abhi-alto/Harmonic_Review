6ad68c14f5d10802813f7eea4178c893ae8e234f
unknown <abhisheksrivastava9719@gmail.com>
2022-07-13 09:17:03 +0530

    [Abhishek] added the master branch containing Harmonic Value problem

---
+++ Harmonic_number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonic
+{
+    internal class Harmonic_number
+    {
+        public int number;                      //class variable
+        public Harmonic_number(int number)              //parameterized constructor
+        {
+            this.number = number;                   //initializing the value
+        }
+        public double getHarmonic()                 //function to get harmonic value for a number
+        {
+            double sum_harmonic = 0.0;                  //for the sum of harmonic value after each iteration
+            for(int i = 1; i <= this.number; i++)       //for each value till the number enteerd
+            {
+                if(i< number)
+                Console.Write("1/"+i+"+");
+                else
+                    Console.WriteLine("1/"+i);
+                sum_harmonic = sum_harmonic + (1.0 / i);                
+            }
+            Console.WriteLine("Harmonic value =" + sum_harmonic);
+            return sum_harmonic;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Harmonic;
+Console.WriteLine("Welcome to the harmonic number problem");
+Console.WriteLine("Enter a positive number you have to get harmonic value of");
+int num=Convert.ToInt16(Console.ReadLine());
+if(num <= 0)                    //for number less than or equal to zero
+{
+    Console.WriteLine("Wrong input given");
+}
+else                        //for numbers greater than zero
+{
+    Harmonic_number GET = new Harmonic_number(num);             //calling the class and giving the value through constructor
+    Console.WriteLine("Harmonic value for the number " + num + " is " + (GET.getHarmonic()));           //printing and calling the Harmonic value function
+}
